b739c877e5f3c8ce2e01c7678af18b887a2ceb7c
Elisa Andrade <elisalvsan@gmail.com>
2021-11-07 00:32:15 -0300

    update footer2

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="footer navbar-bottom py-3 bg-dark">
+        <footer className="footer navbar-fixed-bottom py-3 bg-dark">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/elisalvsan" target="_blank" rel="noreferrer">Elisa Andrade</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
